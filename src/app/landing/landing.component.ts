93f22e49f5bf4a75156fe9ec5ab31b5c860b1553
pacomauret <francisco.fariasm@sansano.usm.cl>
2021-12-14 23:49:28 -0300

    landing and search

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 declare var $: any
+const bootstrap = require('bootstrap')
 
 @Component({
   selector: 'app-landing',
@@ -9,14 +10,38 @@ declare var $: any
 export class LandingComponent implements OnInit {
   id: string = ''
   count = [1, 2, 3];
+  visual =[
+    {id:1,titulo:"Reid et Al[1]",
+    imagen:"../../assets/images/1D Color Nodos.gif",
+    descripcion:"Esta visualizacion se utiliza en [1] con el objetivo de poder distinguir como se distribuye el flujo entre los diferentes nodos al mostrar mediante flechas direccionadas. se destaca esta visualizacion debido a la simplicidad que tiene para mostrar informacion.",
+    propiedades:'Transacciones',
+    canales:'Direccion de aristas',
+    estandar:'se utiliza el color del nodo en las aristas para demostrar al emisor'},
+    {id:2,titulo:"BitConeView[2]",
+    imagen:"../../assets/images/2D Tam Ari- Col Nod.gif",
+    descripcion:"Esta visualizacion se utiliza en [1] con el objetivo de poder distinguir como se distribuye el flujo entre los diferentes nodos al mostrar mediante flechas direccionadas. se destaca esta visualizacion debido a la simplicidad que tiene para mostrar informacion.",
+    propiedades:'Transacciones',
+    canales:'Direccion de aristas',
+    estandar:'se utiliza el color del nodo en las aristas para demostrar al emisor'},
+    {id:3,titulo:"BlockChain Explorer[1]",
+    imagen:"../../assets/images/3D Color nodo - Color Arista - Tamaño nodo.gif",
+    descripcion:"Esta visualizacion se utiliza en [1] con el objetivo de poder distinguir como se distribuye el flujo entre los diferentes nodos al mostrar mediante flechas direccionadas. se destaca esta visualizacion debido a la simplicidad que tiene para mostrar informacion.",
+    propiedades:'Transacciones',
+    canales:'Direccion de aristas',
+    estandar:'se utiliza el color del nodo en las aristas para demostrar al emisor'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    $("*").tooltip('enable');
+
   }
 
   closealert() {
-
     $(".alert").fadeOut();
   }
 
 }
+
+
+
